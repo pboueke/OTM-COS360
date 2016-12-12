ad04f02cf8d4e93565d8e18fe42a3e92628d194d
Pedro Boueke <phboueke@gmail.com>
2016-12-12 04:35:45 -0200

    gradient descent working for f1

---
+++ src/gradient_descent.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <cmath>
+#include <cstring>
+
+using namespace std;
+
+void df1(double x, double y, double* out) {
+  out[0] = (2 * log (x)) / (x * (1 + log(x) * log (x) + log(y) * log(y)));
+  out[1] = (2 * log (y)) / (y * (1 + log(y) * log (y) + log(x) * log(x)));
+}
+
+int main()
+{
+  double old_arr [2] = {0, 0};
+  // starting point
+  double new_arr [2] = {10, 10};
+  // step size
+  double gamma = 0.01;
+  // precision neede for stop
+  double precision = 0.00001;
+  // max iterations
+  int max_iterations = 100000;
+  // counter
+  int current_iteration = 0;
+  // gradient storage
+  double grad [2] = {0, 0};
+
+  cout << "Starting Gradient descent.\nParameters:" << endl;
+  cout << "Gamma: " << gamma << endl;
+  cout << "Precision: " << precision << endl;
+  cout << "Max number of iterations: " << max_iterations << endl;
+  cout << "Starting..." << endl;
+
+  //main loop
+  while (
+          (    (new_arr[0] - old_arr[0] > precision)
+            && (new_arr[1] - old_arr[1] > precision)
+          )
+          || current_iteration < max_iterations
+        )
+  {
+    //old_arr = new_arr;
+    memcpy(&old_arr, &new_arr, 2 * sizeof(double));
+    // calculate new derivate
+    df1(old_arr[0], old_arr[1], grad);
+    new_arr[0] += -gamma * grad[0];
+    new_arr[1] += -gamma * grad[1];
+
+    if (current_iteration%1000 == 0)
+    {
+      cout << "Iteration: " << current_iteration << endl;
+    }
+    current_iteration += 1;
+  }
+
+  cout << "Proccess finalized for f1. Result: (" << new_arr[0] << ", " << new_arr[1] << ")" << endl;
+  cout << "Total iterations for f1: " << current_iteration << endl;
+
+  cout << "Exiting..." << endl;
+  //cin.ignore();
+
+  return 0;
+}
