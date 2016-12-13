7654ccf425b37e486c0cea57a27455e74d6bdde6
Pedro Boueke <phboueke@gmail.com>
2016-12-13 20:17:48 -0200

    Added stop conditions to gradient

--- src/gradient_descent_golden_search.cpp
+++ src/gradient_descent_golden_search.cpp
@@ -1,32 +1,42 @@
+//#include "stdafx.h"
 #include <iostream>
 #include <cmath>
-#include <cstring>
+#include <string>
 #include "string.h"
+
+#define DELTAF false
+#define LASTX false
+#define ITERX true
+
+#define DELTAF_PRECISION 0.0000001
+#define PRECISION 0.0000001
+#define MAX_ITERATIONS 100000
+#define GAMMA 0.01
 
 using namespace std;
 
-double f1 (double x, double y)
+double f1(double x, double y)
 {
-  return log(1 + log(x) * log(x) + log(y) * log(y));
+	return log(1 + log(x) * log(x) + log(y) * log(y));
 }
 
-double f2 (double x, double y)
+double f2(double x, double y)
 {
-  return log(1 + x * x + (x * x - y) * (x * x - y));
+	return log(1 + x * x + (x * x - y) * (x * x - y));
 }
 
 void df1(double x, double y, double* out) {
-  // x   (2 log(x))/(x (log^2(x) + log^2(y) + 1))
-  out[0] = (2 * log (x)) / (x * (1 + log(x) * log (x) + log(y) * log(y)));
-  // y   (2 log(y))/(y (log^2(x) + log^2(y) + 1))
-  out[1] = (2 * log (y)) / (y * (1 + log(y) * log (y) + log(x) * log(x)));
+	// x   (2 log(x))/(x (log^2(x) + log^2(y) + 1))
+	out[0] = (2 * log(x)) / (x * (1 + log(x) * log(x) + log(y) * log(y)));
+	// y   (2 log(y))/(y (log^2(x) + log^2(y) + 1))
+	out[1] = (2 * log(y)) / (y * (1 + log(y) * log(y) + log(x) * log(x)));
 }
 
 void df2(double x, double y, double* out) {
-  // x   (2 (2 x^3 - 2 x y + x))/((x^2 - y)^2 + x^2 + 1)
-  out[0] = (2*(2*x*x*x - 2*x*y + x))/(pow(x*x - y, 2) + x*x + 1);
-  // y  -(2 (x^2 - y))/((x^2 - y)^2 + x^2 + 1)
-  out[1] = -1 * (2*(x*x - y))/(pow(x*x - y, 2) + x*x + 1);
+	// x   (2 (2 x^3 - 2 x y + x))/((x^2 - y)^2 + x^2 + 1)
+	out[0] = (2 * (2 * x*x*x - 2 * x*y + x)) / (pow(x*x - y, 2) + x*x + 1);
+	// y  -(2 (x^2 - y))/((x^2 - y)^2 + x^2 + 1)
+	out[1] = -1 * (2 * (x*x - y)) / (pow(x*x - y, 2) + x*x + 1);
 }
 
 double goldenSectionSearch(double precision, double in_a, double in_b, double *x, double *dx, double(*f) (double, double))
@@ -67,69 +77,78 @@ double goldenSectionSearch(double precision, double in_a, double in_b, double *x
 	return (u + v) / 2;
 }
 
-void gradient (string name, double precision, double gamma, int max_iterations, double *out, void (*d_function)(double, double, double*), double (*function) (double, double))
+void gradient(string name, double precision, double gamma, int max_iterations, double *out, void(*d_function)(double, double, double*), double(*function) (double, double))
 {
-  int current_iteration = 0;
-  double grad [2] = {0, 0};
-  double tmp [2] = {0, 0};
-  double last_out [2] = {0, 0};
-  out[0] = out[1] = 10;
+	int current_iteration = 0;
+	double grad[2] = { 0, 0 };
+	double tmp[2] = { 0, 0 };
+	double last_out[2] = { 0, 0 };
 
-  cout << "Starting Gradient descent.\nParameters:" << endl;
-  cout << "Gamma: " << gamma << endl;
-  cout << "Precision: " << precision << endl;
-  cout << "Max number of iterations: " << max_iterations << endl;
-  cout << "Starting..." << endl;
-  //main loop
-  while (current_iteration < max_iterations)
-  {
-    if (current_iteration%100000 == 0)
-    {
-      cout << "Iteration: " << current_iteration << ". Result: (" << out[0] << ", " << out[1] << ")" << endl;
-    }
-    // calculate new derivate
-    d_function(out[0], out[1], grad);
-    tmp[0] += -gamma * grad[0];
-    tmp[1] += -gamma * grad[1];
+	cout << "Starting Gradient descent.\nParameters:" << endl;
+	cout << "Gamma: " << gamma << endl;
+	cout << "Precision: " << precision << endl;
+	cout << "Max number of iterations: " << max_iterations << endl;
+	cout << "Starting..." << endl;
+	//main loop
+	while (true)
+	{
+		if (current_iteration % 10000 == 0)
+		{
+			cout << "Iteration: " << current_iteration << ". Result: (" << out[0] << ", " << out[1] << ")" << endl;
+		}
+		// calculate new derivate
+		d_function(out[0], out[1], grad);
+		tmp[0] += -gamma * grad[0];
+		tmp[1] += -gamma * grad[1];
 
-    // golden section search
-    double t = goldenSectionSearch(precision, 0, 10, out, tmp, function);
+		// golden section search
+		double t = goldenSectionSearch(precision, 0, 10, out, tmp, function);
 
-    last_out[0] = out[0];
-    last_out[1] = out[1];
+		last_out[0] = out[0];
+		last_out[1] = out[1];
 
-    out[0] = out[0] + t * tmp[0];
-    out[1] = out[1] + t * tmp[1];
+		out[0] = out[0] + t * tmp[0];
+		out[1] = out[1] + t * tmp[1];
 
-    current_iteration += 1;
-
-    if ((abs(out[0] - last_out[0]) < precision) && (abs(out[1] - last_out[1]) < precision))
-    {
-      break;
-    }
-  }
-  cout << "Proccess finalized for " << name <<". Result: (" << out[0] << ", " << out[1] << ")" << endl;
-  cout << "Total iterations: " << current_iteration << endl;
+		current_iteration += 1;
+		if ((current_iteration > max_iterations) && ITERX)
+		{
+			break;
+		}
+		if ((abs(function(out[0], out[1]) - function(last_out[0], last_out[1])) < DELTAF_PRECISION) && DELTAF)
+		{
+			break;
+		}
+		if (((abs(out[0] - last_out[0]) < precision) && (abs(out[1] - last_out[1]) < precision)) && LASTX)
+		{
+			break;
+		}
+	}
+	cout << "Proccess finalized for " << name << ". Result: (" << out[0] << ", " << out[1] << ") Value: " << function(out[0], out[1]) << endl;
+	cout << "Total iterations: " << current_iteration << endl;
 }
 
 int main()
 {
-  // starting point
-  double new_arr [2] = {10, 10};
-  // step size
-  double gamma = 0.01;
-  // precision needed for stop
-  double precision = 0.0000001;
-  // max iterations
-  int max_iterations = 1000000;
+	// starting point
+	double new_arr[2];
+	// step size
+	double gamma = GAMMA;
+	// precision needed for stop
+	double precision = PRECISION ;
+	// max iterations
+	int max_iterations = MAX_ITERATIONS;
 
-  gradient("F1",precision, gamma, max_iterations, new_arr, df1, f1);
+	new_arr[0] = 0.5;
+	new_arr[1] = 0.5;
+	gradient("F1", precision, gamma, max_iterations, new_arr, df1, f1);
+	
+	new_arr[0] = 0.5;
+	new_arr[1] = 0.5;
+	gradient("F2", precision, gamma, max_iterations, new_arr, df2, f2);
 
-  // is F2 unimodal?
-  gradient("F2",precision, gamma, max_iterations, new_arr, df2, f2);
+	cout << "Exiting..." << endl;
+	cin.ignore();
 
-  cout << "Exiting..." << endl;
-  //cin.ignore();
-
-  return 0;
+	return 0;
 }
